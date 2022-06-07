e4652e30d4a1734eacf71dd41809e238a8b075ff
HimanshuS0111 <97181061+HimanshuS0111@users.noreply.github.com>
2022-06-07 17:07:02 +0530

    Add files via upload

---
+++ ProductRepo.java
@@ -0,0 +1,13 @@
+package com.assignment;
+import java.util.*;
+
+public class ProductRepo {
+	static Product[] arr = { new Product (101 , "Apple" , 25.0, "Fruit"),
+			                  new Product (102," Samsung ", 10500.0, "Electronic"),
+			                  new Product(103 , "Jockey " , 150.0 , "cloths"),
+			                  new Product (104 , "Puma " , 1000.0, "Sports"),
+			                  new Product(105 , "Spetula", 50.97, "Utensils")};
+	
+	 		  
+	  }
+
